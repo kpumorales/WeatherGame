1310ebbe69d2dc6540aa1edd48bf738938df0cad
Ulises <ulisexmorales@gmail.com>
2016-10-11 17:18:15 -0500

    ola

---
+++ app/src/main/java/xyz/ulisesprofe/weathergame/main.java
@@ -0,0 +1,50 @@
+package xyz.ulisesprofe.weathergame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button btn1 = (Button) findViewById(R.id.PlayButton);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(main.this, Game.class);
+                startActivity(i);
+            }
+        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exitmenu:
+                finish();
+                break;
+            case R.id.helpmenu:
+                Intent intent = new Intent(getApplicationContext(), Help.class);
+                startActivity(intent);
+                break;
+            case R.id.creditsmenu:
+                Intent i = new Intent(getApplicationContext(), Credits.class);
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
