1646a54f81f552b0652c9e79009ee436d3b69824
Ulises <ulisexmorales@gmail.com>
2016-10-13 14:20:36 -0500

    renombre variables para mejor entendimiento el main mando el score al win.xml

--- app/src/main/java/xyz/ulisesprofe/weathergame/Score.java
+++ app/src/main/java/xyz/ulisesprofe/weathergame/Score.java
@@ -2,25 +2,20 @@ package xyz.ulisesprofe.weathergame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import static xyz.ulisesprofe.weathergame.R.id.listView;
 
 public class Score extends AppCompatActivity {
-    String nombre;
-    int movimientos;
-    ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.score);
-
-        movimientos= Integer.parseInt(getIntent().getExtras().getString("movimientos"));
-        nombre= getIntent().getExtras().getString("nombre");
-        lista= (ListView) findViewById(R.id.listView);
-
     }
 }
--- app/src/main/java/xyz/ulisesprofe/weathergame/Win.java
+++ app/src/main/java/xyz/ulisesprofe/weathergame/Win.java
@@ -1,10 +1,14 @@
 package xyz.ulisesprofe.weathergame;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.OutputStreamWriter;
 
 public class Win extends AppCompatActivity {
     String nombre,movimientos;
@@ -12,14 +16,28 @@ public class Win extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.win);
-        movimientos= getIntent().getExtras().getString("movimientos");
-        nombre= getIntent().getExtras().getString("nombre");
+        //movimientos= getIntent().getExtras().getString("movimientos");
+        //nombre= getIntent().getExtras().getString("nombre");
+
+        try {
+            OutputStreamWriter fout= new OutputStreamWriter(openFileOutput("marcador.txt", Context.MODE_PRIVATE));
+            fout.write(nombre);
+            fout.close();
+        }
+        catch (Exception ex)
+        {
+            Log.e("Ficheros", "Error al escribir fichero a memoria interna");
+        }
     }
+
+
+
 
     public void score(View view) {
         Intent i = new Intent(Win.this, Score.class);
-        i.putExtra("movimientos",movimientos);
-        i.putExtra("nombre",nombre);
+        //i.putExtra("movimientos",movimientos);
+        //i.putExtra("nombre",nombre);
         startActivity(i);
+
     }
 }
